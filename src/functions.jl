c1a97bcd0cc94f1c142d8226c4a4ac689665ad26
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-31 15:01:00 +0000

    Snapshot of branch master taken on 2019-07-31T11:00:59.673-04:00 from "https://github.com/UnofficialJuliaMirror/PolyJuMP.jl-ddf597a6-d67e-5340-b84c-e37d84115374"

---
+++ src/functions.jl
@@ -0,0 +1,7 @@
+# TODO remove this type piracy when we require MOI v0.9 as we need this fix: https://github.com/JuliaOpt/MathOptInterface.jl/pull/800
+function Base.:*(f::MOI.Utilities.ScalarLike, p::AbstractPolynomialLike)
+    return MultivariatePolynomials.multconstant(f, p)
+end
+function Base.:*(p::AbstractPolynomialLike, f::MOI.Utilities.ScalarLike)
+    return MultivariatePolynomials.multconstant(p, f)
+end
